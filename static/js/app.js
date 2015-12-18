4061a5d43ef9dd5d2753d358d8a2cb6659fed03f
Nalle Rooth <nalle.rooth@gmail.com>
2015-12-19 00:46:30 +0100

    New look implemented, removed zepto and static fonts in favor of google fonts.

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,11 @@
-console.log("It's alive!");
+(function () {
+    // Highlight current menu item
+    var menuItems = document.querySelectorAll('.nav-bar ul a');
+
+    Array.prototype.forEach.call(menuItems, function (link) {
+        var re = new RegExp(link.dataset['matches']);
+        if (re.test(window.location.pathname)) {
+            link.classList.add('active');
+        }
+    });
+}());
