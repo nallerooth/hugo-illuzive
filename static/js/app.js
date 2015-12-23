9b1ff34320593b978f5a22608f7b284734af68e4
Nalle Rooth <nalle.rooth@gmail.com>
2015-12-23 13:27:00 +0100

    Updated fonts, removed broken reference to zepto.js, fixed badly formatted javascript.

--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,7 @@
     var menuItems = document.querySelectorAll('.nav-bar ul a');
 
     Array.prototype.forEach.call(menuItems, function (link) {
-        var re = new RegExp(link.dataset['matches']);
+        var re = new RegExp(link.dataset.matches);
         if (re.test(window.location.pathname)) {
             link.classList.add('active');
         }
