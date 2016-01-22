56ee50470102fe9dfad7af4c9b2b53f7486b37b0
Nalle Rooth <nalle.rooth@gmail.com>
2016-01-22 21:36:02 +0100

    Lots of fixes * Updated size of first h1 element on pages to match the h1 on posts. * Moved footer visual contents into sidebar. * Moved novigation from top to sidebar. * Misc small CSS fixes. * Added javascript to update body min height on pages lacking content.

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,16 @@
 (function () {
+    // Make sure body covers full height of browser
+    function updateBodyMinHeight() {
+        document.querySelector('body').style.minHeight = window.innerHeight + 'px'
+    }
+
+    window.addEventListener('resize', function (evt) {
+        updateBodyMinHeight();
+    });
+
+    // Update body height at page load
+    updateBodyMinHeight();
+
     // Highlight current menu item
     var menuItems = document.querySelectorAll('.nav-bar ul a');
 
