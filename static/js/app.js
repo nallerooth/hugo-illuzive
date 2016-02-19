e0e3c444edbc68fc13c62a06b53e65f7932d6e97
Nalle Rooth <nalle.rooth@gmail.com>
2016-02-19 23:20:54 +0100

    - Moved archive into page/index, where it belongs. - Use proper menu functions instead of a static menu built in HTML - Use relative permalinks everywhere except in og:urls and similar

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 (function () {
     // Make sure body covers full height of browser
     function updateBodyMinHeight() {
-        document.querySelector('body').style.minHeight = window.innerHeight + 'px'
+        document.querySelector('body').style.minHeight = window.innerHeight + 'px';
     }
 
     window.addEventListener('resize', function (evt) {
@@ -10,14 +10,4 @@
 
     // Update body height at page load
     updateBodyMinHeight();
-
-    // Highlight current menu item
-    var menuItems = document.querySelectorAll('.nav-bar ul a');
-
-    Array.prototype.forEach.call(menuItems, function (link) {
-        var re = new RegExp(link.dataset.matches);
-        if (re.test(window.location.pathname)) {
-            link.classList.add('active');
-        }
-    });
 }());
